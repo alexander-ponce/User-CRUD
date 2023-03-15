e1a440a2359703058aa8d104a12a09977323f1d4
alexander-ponce <alex@poncealex.com>
2023-03-15 00:00:46 -0700

    first commit

--- flask_app/controllers/user_routes.py
+++ flask_app/controllers/user_routes.py
@@ -1,63 +1,145 @@
 from flask_app import app
 from flask import Flask, render_template, request, redirect, session
 from flask_app.models.user import Users
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask import flash
 
 @app.route("/")
 def index():
     
-    return render_template("create.html")
+    return render_template("register_login.html")
 
-#included user validation
-@app.route('/input_user', methods = ["POST"])
-def input():
-    data = {
-        "first_name": request.form["first_name"],
-        "last_name" : request.form["last_name"],
-        "email" : request.form["email"]
-    }
-    if not Users.validated_user(request.form):
+
+@app.route("/create/account", methods=["POST"])
+def create_account():
+    # validate the form here ...
+    # create the hash
+    if not Users.validate_user(request.form):
         return redirect ("/")
-    # We pass the data dictionary into the save method from the Friend class.
-    show_user=Users.save(data)
-    # Don't forget to redirect after saving to the database.
-    return redirect(f'/user/show/{show_user}')
-    # return redirect('/display_users')
-
-#Show user by ID
-@app.route('/user/show/<int:id>')
-def show_user(id):
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    # print(pw_hash)
+    # put the pw_hash into the data dictionary
     data = {
-        'id': id
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password" : pw_hash
     }
-    # calling the get_one method and supplying it with the id of the friend we want to get
-    return render_template("read_one.html", user= Users.get_one(data))
+    # Call the save @classmethod on User
+    id = Users.save(data)
+    # store user id into session
+    session['user_id'] = id
+
+    # return redirect ('user/account/{user_in_db}')
+    return redirect ('/user/account')
 
 
-@app.route('/display_users')
-def display():
-    return render_template("/read.html", all_users=Users.get_all())
 
-@app.route('/user/edit/<int:id>')
-def edit_user(id):
-    data = {
-        'id': id
+@app.route('/login', methods=['POST'])
+def login_user():
+    if not Users.validate_login(request.form):
+        return redirect('/')
+    email_data={
+        'email':request.form['login_email']
     }
-    return render_template("/edit_user.html", user= Users.get_one(data))
+    returning_user= Users.get_by_email(email_data)
+    session['user_id']= returning_user.id
+    return redirect('/user/account')
 
-@app.route('/user/update',methods=['POST'])
-def update_users():
-    Users.update(request.form)
-    return redirect('/display_users')
 
-@app.route('/user/delete/<int:id>')
-def delete_user(id):
-    data = {
-        'id': id
+# @app.route("/login", methods=["POST"])
+# def login_account():
+#     # see if the username provided exists in the database
+#     # data = { "email" : request.form["email"] }
+#     user_in_db = Users.get_by_email(request.form)
+#     # user_in_db = Users.get_by_email(request.form['email'])
+#     # user is not registered in the db
+#     if not user_in_db:
+#         flash("Invalid Email/Password")
+#         return redirect('/')
+#     if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+#         # if we get False after checking the password
+#         flash("Invalid Email/Password")
+#         return redirect('/')
+#     # if the passwords matched, we set the user_id into session
+#     session['user_id'] = user_in_db.id
+#     # never render on a post!!!
+#     return redirect('/user/account')
+
+#     return render_template ('post_login.html', user_id = Users.get_one(id_data) )
+
+
+@app.route('/user/account')
+# @app.route('/user/account/<int:id>')
+def user_account():
+    if 'user_id' not in session:
+        return redirect ('/logout')
+    id_data={
+        'id': session['user_id']
     }
-    Users.delete(data)
-    return redirect('/display_users' )
+    user_id= Users.get_one(id_data)
+    return render_template ('post_login.html', user_id = user_id)
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# #included user validation
+# @app.route('/input_user', methods = ["POST"])
+# def input():
+#     data = {
+#         "first_name": request.form["first_name"],
+#         "last_name" : request.form["last_name"],
+#         "email" : request.form["email"]
+#     }
+#     if not Users.validated_user(request.form):
+#         return redirect ("/")
+#     # We pass the data dictionary into the save method from the Friend class.
+#     show_user=Users.save(data)
+#     # Don't forget to redirect after saving to the database.
+#     return redirect(f'/user/show/{show_user}')
+#     # return redirect('/display_users')
+
+# #Show user by ID
+# @app.route('/user/show/<int:id>')
+# def show_user(id):
+#     data = {
+#         'id': id
+#     }
+#     # calling the get_one method and supplying it with the id of the friend we want to get
+#     return render_template("read_one.html", user= Users.get_one(data))
+
+
+# @app.route('/display_users')
+# def display():
+#     return render_template("/read.html", all_users=Users.get_all())
+
+# @app.route('/user/edit/<int:id>')
+# def edit_user(id):
+#     data = {
+#         'id': id
+#     }
+#     return render_template("/edit_user.html", user= Users.get_one(data))
+
+# @app.route('/user/update',methods=['POST'])
+# def update_users():
+#     Users.update(request.form)
+#     return redirect('/display_users')
+
+# @app.route('/user/delete/<int:id>')
+# def delete_user(id):
+#     data = {
+#         'id': id
+#     }
+#     Users.delete(data)
+#     return redirect('/display_users' )
+
+
 
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,15 +1,22 @@
+from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import user 
 from flask import flash
 import re
 EMAIL_REGEX= re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-# model the class after the friend table from our database
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)    # we are creating an object called bcrypt, 
+                        # which is made by invoking the function Bcrypt with our app as an argument
+
+
 class Users:
-    DB = "users_schema"
+    DB = "user_login"
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
             # Now we use class methods to query our database
@@ -26,63 +33,116 @@ class Users:
         return users
     
     @classmethod
-    def get_one(cls, data):
-        query = "SELECT * FROM users WHERE id = %(id)s;"
-        results = connectToMySQL(cls.DB).query_db(query, data)
-        return cls(results[0])
+    def get_by_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        # Didn't find a matching user
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
 
     @classmethod
     def save(cls, data ):
         query = """
                 INSERT into users 
-                (first_name, last_name, email, created_at, updated_at) 
+                (first_name, last_name, email, password) 
                 VALUES 
-                ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() )
+                ( %(first_name)s , %(last_name)s , %(email)s ,  %(password)s)
         
         """
         # data is a dictionary that will be passed into the save method from server.py
         result = connectToMySQL(cls.DB).query_db(query,data)
         return result
 
-
     @classmethod
-    def update(cls,data):
-        query = """
-                UPDATE users
-                set first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s
-                WHERE id = %(id)s;
-        """
-                #Semicolon not neeed, only best practice
-        result = connectToMySQL(cls.DB).query_db(query,data)
-        return result
-    
-    @classmethod
-    def delete(cls, data): #double check on this delete(id)
-        query = """
-                DELETE FROM users
-                WHERE id = %(id)s;
-        
-        """
-        # data= {"id":id }
-        
-        result = connectToMySQL(cls.DB).query_db(query,data)
-        return result
+    def get_one(cls, data):
+        query = """SELECT * FROM users WHERE id = %(id)s;"""
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(results[0])
 
     @staticmethod
-    def validated_user(form_data):
+    def validate_user(form_data):
         is_valid = True
         if len(form_data["first_name"]) < 3:
-            flash("First name must be at least 3 characters.")
+            flash("First name must be at least 3 characters.", "register")
             is_valid = False
         if len(form_data["last_name"]) < 3:
-            flash("Last name must be at least 3 characters.")
+            flash("Last name must be at least 3 characters.", "register")
             is_valid = False
         if not EMAIL_REGEX.match(form_data["email"]):
-            flash ("Invalid email address.")
+            flash ("Invalid email address.", "register")
             is_valid = False
+        if len(form_data["password"]) < 8:
+            flash ("Password must be at least 8 characters", "register")
+            is_valid = False
+        if form_data['conf_password'] != form_data['password']:
+            flash("Password and confirm password must match!", "register")
+            is_valid=False
         
         return is_valid
 
+    @staticmethod
+    def validate_login(form_data):
+        is_valid= True
+        
+        data= { "email": form_data["login_email"]}
+        valid_user = Users.get_by_email(data)
+        if not valid_user:
+            flash('Invalid Crendentials', "login")
+            is_valid=False
+        if valid_user:
+            if not bcrypt.check_password_hash(valid_user.password, form_data['login_password']):
+                flash('Invalid Credentials',"login")
+                is_valid=False
+        return is_valid
 
-        # return connectToMySQL('first_flask').query_db( query, data )
+
+
+    # @staticmethod
+    # def validated_login(form_data):
+    #     is_valid = True
+    #     query = """SELECT * FROM users WHERE email = %(email)s;"""
+    #     results = connectToMySQL(Users.DB).query_db(query,form_data)
+    #     if len(results) >= 1:
+    #         flash("Email already taken.","register")
+    #         is_valid = False
+    #     if not EMAIL_REGEX.match(form_data['email']):
+    #         flash("Invalid Email!!!","register")
+    #     if len(form_data["first_name"]) < 3:
+    #         flash("First name must be at least 3 characters.")
+    #         is_valid = False
+    #     if len(form_data["last_name"]) < 3:
+    #         flash("Last name must be at least 3 characters.")
+    #         is_valid = False
+    #     return is_valid
+
+
+    
+
+
+    # @classmethod
+    # def update(cls,data):
+    #     query = """
+    #             UPDATE users
+    #             set first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s
+    #             WHERE id = %(id)s;
+    #     """
+    #             #Semicolon not neeed, only best practice
+    #     result = connectToMySQL(cls.DB).query_db(query,data)
+    #     return result
+    
+    # @classmethod
+    # def delete(cls, data): #double check on this delete(id)
+    #     query = """
+    #             DELETE FROM users
+    #             WHERE id = %(id)s;
+        
+    #     """
+    #     # data= {"id":id }
+        
+    #     result = connectToMySQL(cls.DB).query_db(query,data)
+    #     return result
+
+
 
